7a9544c6f5680eb5e37afbb7a6cee7c70af13251
PitoyoAidropHunter <pitoyoaidrophunter@gmail.com>
2025-11-22 03:04:00 +0700

    add new component : button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,46 @@
-const Button = () => {
+import { NavLink } from "react-router";
+
+interface ButtonProps {
+  title: string;
+  color?: "dark" | "light";
+  type?: "button" | "submit" | "reset";
+  href?: string;
+}
+
+const Button = ({
+  title,
+  color = "dark",
+  type = "button",
+  href = "",
+}: ButtonProps) => {
+  const darkColor =
+    "bg-gray-800 text-white hover:bg-gray-900 transition-all ease-in-out duration-200";
+  const lightColor =
+    "bg-gray-200 text-gray-800 hover:bg-gray-300 transition-all ease-in-out duration-200";
+  const defaultStyle =
+    "font-semibold px-6 py-3 cursor-pointer text-lg rounded-lg";
+
+  if (href) {
+    return (
+      <NavLink
+        to={href}
+        className={`${defaultStyle} ${
+          color === "dark" ? darkColor : lightColor
+        } `}
+      >
+        {title}
+      </NavLink>
+    );
+  }
+
   return (
-    <button className="bg-gray-800 font-semibold text-white px-4 py-2 rounded">
-      Click Me
+    <button
+      type={type}
+      className={`${defaultStyle} ${
+        color === "dark" ? darkColor : lightColor
+      } `}
+    >
+      {title}
     </button>
   );
 };
